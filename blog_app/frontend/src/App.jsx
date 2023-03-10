c9942320165320a1cef9aae55c4c64be4588cd42
mayur <mayurkukde84@gmail.com>
2023-03-10 21:48:21 +0530

    add frontend setup

---
+++ blog_app/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RegisterPage from './pages/RegisterPage'
+import './App.css'
+const App = () => {
+  return (
+    <div>
+      <RegisterPage />
+    </div>
+  )
+}
+
+export default App
---
+++ blog_app/frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './register.css'
+const RegisterPage = () => {
+  return (
+    <div>
+        <form>
+            <h3>Sign Up</h3>
+            <label>Username</label>
+            <input />
+            <label>Email</label>
+            <input />
+            <label>Password</label>
+            <input />
+            <label>PasswordConfirm</label>
+            <input />
+            <button>Submit</button>
+
+
+        </form>
+    </div>
+  )
+}
+
+export default RegisterPage
