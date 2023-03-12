acd4142b89be16e207ab39d86a72d81e5b63daff
mayur <mayurkukde84@gmail.com>
2023-03-12 20:34:01 +0530

    add

--- blog_app/frontend/src/App.jsx
+++ blog_app/frontend/src/App.jsx
@@ -1,23 +1,11 @@
-import React, { useState, createContext } from "react";
-import RegisterPage from "./pages/RegisterPage";
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import Home from "./pages/Home";
-
+import React from "react";
+import Register from "./Register";
 
 const App = () => {
- 
   return (
-    <div className="container-center">
-    
-        <Routes>
-          <Route path="/" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-    
-    </div>
+    <main className="App">
+      <Register />
+    </main>
   );
 };
 
