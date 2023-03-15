9c7840cda1164c09a66400bb228016981d14d510
mayur <mayurkukde84@gmail.com>
2023-03-15 22:47:21 +0530

    update

--- blog_app/frontend/src/components/RequireAuth.jsx
+++ blog_app/frontend/src/components/RequireAuth.jsx
@@ -4,12 +4,10 @@ import useAuth from "../hooks/useAuth";
 const RequireAuth = ({ allowedRoles }) => {
     const { auth } = useAuth();
     const location = useLocation();
-    console.log(allowedRoles)
-    
-console.log(auth)
+    console.log(auth?.roles,'auth')
     return (
+        auth?.roles?.find(role => allowedRoles?.includes(role))
       
-     allowedRoles?.includes(auth?.roles) 
             ? <Outlet />
             : auth?.user
                 ? <Navigate to="/unauthorized" state={{ from: location }} replace />
