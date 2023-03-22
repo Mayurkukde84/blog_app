c5b1e2905298f029e47bc53abf68708e816691b8
mayur <mayurkukde84@gmail.com>
2023-03-22 23:46:51 +0530

    databse is added

--- authWithAxios/backend/server.js
+++ authWithAxios/backend/server.js
@@ -3,6 +3,14 @@ const express = require("express");
 const app = express();
 const path = require('path')
 const PORT = process.env.PORT;
+const connectdb = require('./config/connectdb')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
 app.use('/',express.static(path.join(__dirname,'public')))
 app.use('/',require('./routes/root'))
 app.use('*',(req,res)=>{
@@ -15,6 +23,15 @@ app.use('*',(req,res)=>{
         res.type('txt').send('404 Not found')
     }
 })
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
-});
+connectdb()
+mongoose.connection.on('open',()=>{
+    console.log('mongodb is connected')
+    app.listen(PORT, () => {
+        console.log(`server is running on port ${PORT}`);
+      });
+})
+
+mongoose.connection.on('err',err=>{
+console.log(err)
+})
+
