0475aa14db3becfcb78a7f3e6f3b450bd2607d72
mayur <mayurkukde84@gmail.com>
2023-03-24 22:54:52 +0530

    update

--- authWithAxios/backend/controllers/authController.js
+++ authWithAxios/backend/controllers/authController.js
@@ -1,9 +1,11 @@
+const mongoose = require("mongoose");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const {promisify} = require('util')
 
 const signup = asyncHandler(async (req, res) => {
-  const { username, password, passwordConfirm, role, email } = req.body;
+  const { username, role, email, password, passwordConfirm } = req.body;
 
   const user = await User.create({
     username,
@@ -12,84 +14,95 @@ const signup = asyncHandler(async (req, res) => {
     passwordConfirm,
     role,
   });
+
   const token = await jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
-  const cookiesOption = {
+  const cookieOptions = {
     expiresIn: new Date(
       Date.now() + process.env.JWT_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     secure: false,
     httpOnly: true,
   };
+  res.cookie("jwt", token, cookieOptions);
   user.password = undefined;
-  res.cookie("jwt", token, cookiesOption);
 
   res.status(200).json({
     status: "success",
     token,
-    user,
+    data: {
+      user,
+    },
   });
 });
 
 const login = asyncHandler(async(req,res)=>{
-    const {email,password} = req.body
-    if(!email || !password){
-        return res.status(400).json({message:"please fill login and password"})
-    }
+  const {email,password} = req.body
 
-    const user = await User.findOne({email}).select("+password")
+  if(!email || !password){
+    return res.status(400).json({message:'please filled login and password'})
+  }
 
-    if(!user || !(await user.checkpassword(password,user.password))){
-        return res.status(400).json({message:"Incorrect email and password"})
-    }
+  const user = await User.findOne({email}).select('+password')
 
-    const token = await jwt.sign({id:user._id},process.env.JWT_SECRET,
-        {
-            expiresIn:process.env.JWT_EXPIRES_IN
-        })
-    
-    const cookiesOptions = {
-        expiresIn:new Date(
-            Date.now() + process.env.JWT_EXPIRES_IN *24 *60*60*1000
-        ),
-        secure:false,
-        httpOnly:true
-    }
-    user.password = undefined
-    res.cookie('jwt',token,cookiesOptions)
-    res.status(200).json({
-        status: "success",
-        token,
-        user,
-      });
+  if(!user || !(await user.checkPassword(password,user.password))){
+    res.status(400).json({
+      status:"failed",
+      message:'Incorrect wmail or password'
+    })
+  }
+ const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{
+  expiresIn: process.env.JWT_EXPIRES_IN
+ })
+
+const cookieOptions = {
+  expiresIn: new Date(
+    Date.now() + process.env.JWT_EXPIRES_IN*24*60*60*1000
+  ) ,
+   secure: false,
+  httpOnly: true,
+}
+res.cookie("jwt",token,cookieOptions)
+user.password = undefined
+
+res.status(200).json({
+  status:'success',
+  token,
+  user
 })
 
-const protect = asyncHandler(async(req,res)=>{
-    let token;
-    if(
-        req.headers.authorization &&
-        req.headers.authorization.startsWith("Bearer")
-    ){
-        token = req.headers.authorization.split(" ")[1]
-    }
-    if(!token){
-        return res.status(401).json({message:'you are not logged in to get access'})
-    }
+})
 
-    const decoded = await promisify(jwt.verify)(token,process.env.JWT_SECRET);
-    console.log(decoded)
+const protect = asyncHandler(async(req,res,next)=>{
+  let token;
+  if(
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ){
+    token = req.headers.authorization.split(" ")[1];
+  }
 
-    const currentUser = await User.findById(decoded.id);
-    if(!currentUser.changedPasswordAfter(decoded.iat)){
-        return res.status(401).json({
-            message:"User recently changed password! please login again"
-        })
-    }
+  if(!token){
+    return res.status(401).json({message:"your are not logged in to get access"})
+  }
+  const decoded = await promisify(jwt.verify)(token,process.env.JWT_SECRET)
+  console.log(decoded)
 
-    req.user = currentUser
-    next()
+  const currentUser = await User.findById(decoded.id);
+
+  if(!currentUser){
+    return res.status(401).json({
+      message:"The user belongig to the token does not exist"
+    })
+  }
+  if(currentUser.changedPasswordAfter(decoded.iat)){
+    return res.status(401).json({message:'User recently changed password ! Please log in again'})
+  }
+  req.user = currentUser
+  next()
+
 })
 
 module.exports = {
--- authWithAxios/backend/models/userModel.js
+++ authWithAxios/backend/models/userModel.js
@@ -46,33 +46,19 @@ const userSchema = new mongoose.Schema({
     select:false
   },
   passwordChangedAt: Date,
+  passwordResetToken:String,
+  passwordResetExpires:Date,
 });
 
 userSchema.pre('save',async function(next){
-  if(!this.isModified('password')) return next()
+  if(!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password,12)
   this.passwordConfirm = undefined
   next()
 })
 
-userSchema.pre('save',function(next){
-  if(!this.isModified('password')|| this.isNew) return next()
-  this.passwordChangedAt = Date.now() - 1000;
-  next()
-})
-
-userSchema.methods.checkpassword = async function(
-  candidatePassword,userPassword){
-  return await bcrypt.compare(candidatePassword,userPassword)
+userSchema.methods.checkPassword =async function(cureentPassword,userPassword){
+return await bcrypt.compare(cureentPassword,userPassword)
 }
-
-userSchema.methods.changedPasswordAfter = function(JWTTimestamp){
-  if(this.passwordChangedAt){
-    const changeTimestamp = parseInt(this.passwordChangedAt.getTime()/1000,10)
-    return JWTTimestamp < changeTimestamp
-  }
-  return false
-}
-
 
 module.exports = mongoose.model('User',userSchema)
